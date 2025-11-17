6ceeebba75b414d5a7fe2b608195d69a9852e1a3
Sameh AlSayed Rajab <samehsr.work@gmail.com>
2025-11-17 08:34:34 +0300

    HW (Week 3) (17-Nov-2025)

--- PushCode.ps1
+++ PushCode.ps1
@@ -1,19 +1,14 @@
 # Get today's date
 $today = Get-Date
 
-# Calculate week number using ISO standard
-$calendar = [System.Globalization.DateTimeFormatInfo]::CurrentInfo.Calendar
-$week = $calendar.GetWeekOfYear($today,
-    [System.Globalization.CalendarWeekRule]::FirstFourDayWeek,
-    [DayOfWeek]::Monday)
+# Week of month = ((day-1) / 7) + 1
+$weekOfMonth = [math]::Ceiling($today.Day / 7)
 
 # Format date
 $todayFormatted = $today.ToString("dd-MMM-yyyy")
 
 # Commit message
-$msg = "HW (Week $week) ($todayFormatted)"
-
-
+$msg = "HW (Week $weekOfMonth) ($todayFormatted)"
 
 git add .
 git commit -m "$msg"
