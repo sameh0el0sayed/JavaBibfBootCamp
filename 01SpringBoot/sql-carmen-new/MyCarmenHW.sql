1635c585f1c1eaf34b50b6ab0a1867b4ff1811ec
Sameh AlSayed Rajab <samehsr.work@gmail.com>
2025-12-17 13:47:59 +0300

    HW (Week 3) (17-Dec-2025)

---
+++ 01SpringBoot/sql-carmen-new/MyCarmenHW.sql
@@ -0,0 +1,14 @@
+SELECT 
+    c.name,
+    c.population,
+    cl.language,
+    cl.countrycode ,
+	cty.name as "City Name"
+FROM public.country c
+JOIN public.countrylanguage cl
+    ON c.code = cl.countrycode
+	JOIN public.city cty
+    ON c.code = cty.countrycode
+WHERE c.population > 0
+ORDER BY c.population ASC
+LIMIT 1;
