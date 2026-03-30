a7c3d8f9e567ae278050085d073c6717a38febdd
Sameh AlSayed Rajab <samehsr.work@gmail.com>
2026-03-30 15:10:50 +0300

    HW (Week 5) (30-Mar-2026)

---
+++ Ticket_System_Project3/src/main/java/com/ga/TicketSystemProject3/TicketSystemProject3Application.java
@@ -0,0 +1,13 @@
+package com.ga.TicketSystemProject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSystemProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketSystemProject3Application.class, args);
+	}
+
+}
